e52c1ea18ada0d496783195781e81e2350ea5838
Dawit Abraha <akdawit1@gmail.com>
2023-01-03 14:09:15 +0000

    Created available meals list and styled it

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   return (
       <Fragment>
           <header className={classes.header}>
-              <hi>React Meals</hi>
+              <hi>Dawit Meals</hi>
               <HeaderCartButton />
           </header>
           <div className={classes['main-image']}>
